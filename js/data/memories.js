a5df87116d51ee8cc7a56e603630c7a3469f558c
IqbalAdri <qbladri@gmail.com>
2025-02-02 18:16:58 +0700

    first commit

---
+++ js/data/memories.js
@@ -0,0 +1,30 @@
+export const memories = [
+    {
+        src: "../photos/1.png",
+        alt:"Our memouries 1"
+    },
+    {
+        src: "../photos/2.png",
+        alt:"Our memouries 2"
+    },
+    {
+        src: "../photos/3.png",
+        alt:"Our memouries 3"
+    },
+    {
+        src: "../photos/4.png",
+        alt:"Our memouries 4"
+    },
+    {
+        src: "../photos/5.png",
+        alt:"Our memouries 5"
+    },
+    {
+        src: "../photos/6.png",
+        alt:"Our memouries 6"
+    },
+    {
+        src: "../photos/7.png",
+        alt:"Our memouries 7"
+    },
+];
---
+++ js/photos.js
@@ -0,0 +1,30 @@
+export const memories = [
+    {
+        src: "../photos/1.png",
+        alt: "Love Memory 1"
+    },
+    {
+        src: "../photos/2.png",
+        alt: "Love Memory 2"
+    },
+    {
+        src: "../photos/3.png",
+        alt: "Love Memory 3"
+    },
+    {
+        src: "../photos/4.png",
+        alt: "Love Memory 4"
+    },
+    {
+        src: "../photos/5.png",
+        alt: "Love Memory 5"
+    },
+    {
+        src: "../photos/6.png",
+        alt: "Love Memory 6"
+    },
+    {
+        src: "../photos/7.png",
+        alt: "Love Memory 7"
+    }
+];
